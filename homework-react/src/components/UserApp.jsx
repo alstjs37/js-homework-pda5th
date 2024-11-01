21d995e2d4df7c51f16a59c0d1d47638bf67359b
alstjs37 <kubernetes.io@kakao.com>
2024-11-01 22:31:08 +0900

    feat: react-2.2.2 userApp.jsx

--- homework-react/src/components/UserApp.jsx
+++ homework-react/src/components/UserApp.jsx
@@ -1,16 +1,34 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import ListGroup from "react-bootstrap/ListGroup";
 import { Button, Col, Container, Modal, Row } from "react-bootstrap";
 
+const userPromiseObj = async function getUsers() {
+  const url = 'https://jsonplaceholder.typicode.com/users';
+  const res = await axios.get(url);
+  const data = res.data;
+
+  return data;
+}
+
 export default function UserApp() {
+  const [userData, setUserData] = useState([]);
+  
+  useEffect(() => {
+    userPromiseObj().then((data) => {
+      setUserData(data);
+    })
+  }, []);
+  console.log(userData)
+
   return (
     <div>
       <h1>Users</h1>
 
       <ListGroup>
-        <ListGroup.Item>0</ListGroup.Item>
-        <ListGroup.Item>1</ListGroup.Item>
+        {userData.map((user) => (
+          <ListGroup.Item key={user.id}>{`${user.id}. ${user.username} - ${user.email}`}</ListGroup.Item>
+        ))}
       </ListGroup>
 
     </div>
